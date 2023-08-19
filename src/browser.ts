e85bcb653861118cbb7e2fffe36d34ff9174bb83
Lucas8x <lucasemanuel8x@gmail.com>
2023-08-18 21:21:48 -0300

    feat: join game

--- src/browser.ts
+++ src/browser.ts
@@ -2,7 +2,7 @@ import type { Page } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-import { GAME_URL } from './utilities/constants';
+import * as constants from './utilities/constants';
 import { logger } from './utilities/logger';
 
 puppeteer.use(StealthPlugin());
@@ -15,17 +15,51 @@ export class Browser {
 
   constructor() {}
 
-  private joinGame() {
+  private async joinGame() {
+    try {
+      logger.log('[Browser] Joining game...');
+      if (!this.currentPage) {
+        throw Error('NO CURRENT PAGE');
+      }
+
+      const btn = await this.currentPage.waitForXPath(constants.ENTER_BTN);
+      if (!btn) {
+        throw Error("COULDN'T FIND JOIN BUTTON");
+      }
+
+      await btn.click();
+
+      await this.currentPage.waitForXPath(constants.LOADING_SCREEN, {
+        hidden: true,
+      });
+
+      const input = await this.currentPage.waitForXPath(
+        constants.USERNAME_INPUT
+      );
+      if (!input) {
+        throw Error("COULDN'T FIND INPUT");
+      }
+
+      await input.click();
+      await input.focus();
+
+      await this.currentPage.keyboard.down('Control');
+      await this.currentPage.keyboard.press('A');
+      await this.currentPage.keyboard.up('Control');
+      await this.currentPage.keyboard.press('Backspace');
+
+      await input.type('Toichi');
+    } catch (error) {
+      logger.error('[Browser] Failed to join game', String(error));
+    }
+  }
+
+  public writeAnswers() {
     try {
     } catch (error) {}
   }
 
-  writeAnswers() {
-    try {
-    } catch (error) {}
-  }
-
-  validateAnswers() {
+  public validateAnswers() {
     try {
     } catch (error) {}
   }
@@ -40,23 +74,25 @@ export class Browser {
     } catch (error) {}
   }
 
-  loop() {
+  private loop() {
     try {
     } catch (error) {}
   }
 
-  async launch() {
+  public async launch() {
     try {
       logger.log('[Browser] Launching...');
       const browser = await puppeteer.launch({ headless: false });
+      const pages = await browser.pages();
 
-      let page = await browser.pages()[0];
-      if (browser.pages.length === 0) {
+      let page = pages[0];
+      if (pages.length === 0) {
         page = await browser.newPage();
       }
       this.currentPage = page;
 
-      await page.goto(GAME_URL);
+      await page.goto(constants.GAME_URL);
+      await this.joinGame();
     } catch (error) {
       logger.error('[Browser] Failed to launch', String(error));
     }
--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,7 +1,8 @@
 export const GAME_URL = 'https://stopots.com.br/';
-
 export const COMMAND_PREFIX = '!';
-
 export const COMMAND_DELIMITER = ':';
-
 export const MAX_ANSWER_LENGTH = 20;
+
+export const ENTER_BTN = '//*[@class="login"]/button[@class="enter"]';
+export const LOADING_SCREEN = '//*[@class="load"]';
+export const USERNAME_INPUT = '//*[@class="perfil"]//input';
